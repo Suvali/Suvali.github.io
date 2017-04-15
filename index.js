c7d0fa87e872d526c3fbf687978ba1b170c5b25d
Suvali <Suvali@users.noreply.github.com>
2017-04-15 11:38:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -18,25 +18,28 @@ var foodObj = {
         nutrition: "360 carbohydrates"}
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
 
-        var obj = ["tomato soup", "spaghetti bolognese", "cheese pizza", "caesar salad"];
-        var search = function (searchStr) {findObj = null;
-                                          
-    for (var key in obj) {
-    if (obj.hasOwnProperty(key)) {
-        for (var k in obj[key]) {
-            if (obj[key].hasOwnProperty(k)) {
-                if (searchStr === obj[key][k]) {
-                    findObj = obj[key];
-                    break;
-                }
-            }
-        }
-        if (findObj) {
-            break;
-        }
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
-return findObj;
-}
-  
