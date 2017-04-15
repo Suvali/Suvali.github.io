d0d8fac73bbabd67fa07f3cb72df8e7c5b110816
Suvali <Suvali@users.noreply.github.com>
2017-04-15 10:37:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ undefined
 var foodObj = {
     grilled cheese sandwich: {
         ingredients: "bread, cheese",
-        nutrition: "40g carbohydrates"
+        nutrition: "40g carbohydrates"}
     },
     spaghetti bolognese:  {
         ingredients: "spaghetti, tomato, basil, oregano, olive oil, garlic, onion, salt, pepper",
-        nutrition: "220 carbohydrates"
+        nutrition: "220 carbohydrates"}
     },
     cheese pizza:  {
         ingredients: "pizza dough, tomato sauce, cheese",
-        nutrition: "223 carbohydrates"
+        nutrition: "223 carbohydrates"}
     },
     caesar salad:  {
         ingredients: "romaine lettuce, anchovy sauce, croutons",
-        nutrition: "360 carbohydrates"
+        nutrition: "360 carbohydrates"}
+        
+        var search = function (searchStr) {findObj = null;
+    for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+        for (var k in obj[key]) {
+            if (obj[key].hasOwnProperty(k)) {
+                if (searchStr === obj[key][k]) {
+                    findObj = obj[key];
+                    break;
+                }
+            }
+        }
+        if (findObj) {
+            break;
+        }
+    }
+}
+return findObj;
+}
   
